e2731628add74ea66054341a504540e5ac0d6dbe
servorx <191423123+servorx@users.noreply.github.com>
2026-03-09 15:29:14 -0500

    feat: :sparkles: create new react-tastify

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,11 +1,13 @@
 import { AppRouter } from "./router/AppRouter";
 import { ThemeFloatingToggle } from "./components/ThemeFloatingToggle";
+import { AppToastContainer } from "./components/AppToastContainer";
 
 export default function App() {
   return (
     <>
       <AppRouter />
       <ThemeFloatingToggle />
+      <AppToastContainer />
     </>
   );
 }
